1140602031db3dd88b6dae1b2e5ef9e6ee9d0f84
Arno <arnaud.joubel@gmail.com>
2015-12-26 23:10:08 +0100

    Description: Improve unit test
    Issue: NA
    Author: Arnaud Joubel <arnaud.joubel@gmail.com>
    Review: NA
    Test performed:
    Test result: pass

--- test/spec/itemsimpleSpec.js
+++ test/spec/itemsimpleSpec.js
@@ -6,19 +6,38 @@ describe("ItemSimple", function() {
   });
 
   it("should be initialized", function() {
+    var itemId = itemsimple.getId();
+
     expect(itemsimple.getView()).toEqual(null);
+    expect(itemsimple.getId()).toEqual(itemsimple.id_);
+    expect(itemsimple.isChecked()).toEqual(false);
+    expect(itemsimple.text_).toEqual('');
+    expect(itemsimple.parent_).toEqual(undefined);
   });
 
   describe("when view is build", function() {
     beforeEach(function() {
       itemsimple.buildView();
+      setFixtures(itemsimple.getView());
     });
 
     it("should initialized the view", function() {
-      var itemsimple2 = new ItemSimple();
-      itemsimple2.buildView();
-      console.log(itemsimple2);
-        expect(itemsimple2.getView()).not.toEqual(null);
+      var view = itemsimple.getView();
+      var itemId = itemsimple.getId();
+      expect(view).not.toEqual(null);
+      expect(view.id).toEqual(itemId);
+      expect(view.className).toEqual('sosimplist-item');
+    });
+
+    describe("when item is checked", function() {
+      beforeEach(function() {
+        $('.sosimplist-item-checkbox').trigger('click');
+      });
+
+      it("should checked the checkbox", function() {
+        expect(itemsimple.isChecked()).toEqual(true);
+        expect($('.sosimplist-item-checkbox').is(":checked")).toBe(true);
+      });
     });
   });
 });
