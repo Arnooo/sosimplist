49331b84cdf821184e72274fca80e65297f5b9d7
Arnaud Joubel <arnaud.joubel@supersonicimagine.com>
2016-01-05 17:31:14 +0100

    Description: Add grunt regex-replace to comment DEBUG function in grunt build
    Feature(s) impacted:
    Author: Arnaud J
    Root cause:
    Test performed:
    Test result: Pass
    Reviewer(s):
    Number of findings: 0
    Problem(s): 0
    Resolution(s): 0

--- src/debug.js
+++ src/debug.js
@@ -2,21 +2,21 @@
 
 ///////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////
-function DEBUGCheckNoArgument(inArguments) {
+function//DEBUGCheckNoArgument(inArguments) {
     console.assert(inArguments.length === 0);
 }
 
 ///////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////
-function DEBUGCheckArgumentsAreValids(inArguments, inNbArgument) {
+function//DEBUGCheckArgumentsAreValids(inArguments, inNbArgument) {
     console.assert(inArguments.length === inNbArgument, 'Set ' + inArguments.length + ' arguments in place of ' + inNbArgument);
 
-    DEBUGCheckFirstArgumentsAreValids(inArguments, inNbArgument);
+   //DEBUGCheckFirstArgumentsAreValids(inArguments, inNbArgument);
 }
 
 ///////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////
-function DEBUGCheckFirstArgumentsAreValids(inArguments, inNbArgument) {
+function//DEBUGCheckFirstArgumentsAreValids(inArguments, inNbArgument) {
     for (var i = 0; i < inNbArgument; i++) {
         var arg = inArguments[i];
         var type = typeof arg;
@@ -29,7 +29,7 @@ function DEBUGCheckFirstArgumentsAreValids(inArguments, inNbArgument) {
 
 ///////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////
-function DEBUGAssertValue(inValue) { // tddo rename me
+function//DEBUGAssertValue(inValue) { // tddo rename me
     var type = typeof inValue;
 
     console.assert(type !== 'undefined');
